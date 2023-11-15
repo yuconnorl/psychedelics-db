eef82a5f68e0c24f0cb58b43dc0f6e053a3c6ee0
yuc <yuconnorl@gmail.com>
2023-11-15 16:22:01 +0800

    feat: completed layout card

--- src/components/GridLayoutCard.tsx
+++ src/components/GridLayoutCard.tsx
@@ -6,6 +6,57 @@ import { AspectRatio } from './ui/aspect-ratio'
 import { getAllRecords } from '@/api/general'
 import { Badge } from '@/components/ui/badge'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { resolveMetaTag } from '@/utilities/metaTag'
+
+const GridCard = async ({ slug, title, url, type, language }) => {
+  const meta = await resolveMetaTag(url, slug)
+
+  return (
+    <Card className="relative sm:group-hover:-translate-x-2 sm:group-hover:-translate-y-2 lg:group-hover:-translate-x-3 lg:group-hover:-translate-y-3 transition-transform z-10">
+      <CardHeader>
+        <CardTitle className="leading-normal mb-2">{title}</CardTitle>
+        <div className="flex gap-2">
+          <Badge className="w-fit" variant="secondary">
+            {type}
+          </Badge>
+          <Badge className="w-fit" variant="secondary">
+            {language}
+          </Badge>
+        </div>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-2">
+        <div className="saturate-[0.15] group-hover:saturate-100 transition-[filter]">
+          <AspectRatio ratio={21 / 9}>
+            <Image
+              src={meta.imgUrl}
+              fill
+              alt="Image"
+              sizes="(max-width: 768px) 80vw, (max-width: 1200px) 50vw, 20vw"
+              className="rounded-md object-cover object-center"
+            />
+          </AspectRatio>
+        </div>
+        <p className="line-clamp-2 text-muted-foreground text-base group-hover:text-foreground transition-colors">
+          {meta.description}
+        </p>
+      </CardContent>
+      <CardFooter>
+        <div className="relative flex gap-1.5 items-center text-muted-foreground">
+          <Image
+            src={meta.iconUrl}
+            alt={`Icon of ${title}`}
+            width={20}
+            height={20}
+            className="object-contain"
+          />
+          <Link className="text-sm break-all" href={url}>
+            {url}
+          </Link>
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
 
 const GridLayoutCard = async ({ params }) => {
   const records = await getAllRecords()
@@ -13,55 +64,9 @@ const GridLayoutCard = async ({ params }) => {
 
   return (
     <>
-      {filterRecord.map(({ category, slug, title, url, id, type, language }) => (
+      {filterRecord.map(({ slug, title, url, type, language }) => (
         <div className="relative group h-fit">
-          <Card className="relative sm:group-hover:-translate-x-2 sm:group-hover:-translate-y-2 lg:group-hover:-translate-x-3 lg:group-hover:-translate-y-3 transition-transform z-10">
-            <CardHeader>
-              <CardTitle className="leading-normal mb-2">{title}</CardTitle>
-              <div className="flex gap-2">
-                <Badge className="w-fit" variant="secondary">
-                  {type}
-                </Badge>
-                <Badge className="w-fit" variant="secondary">
-                  {language}
-                </Badge>
-              </div>
-            </CardHeader>
-            <CardContent className="flex flex-col gap-2">
-              <div className="saturate-[0.15] group-hover:saturate-100 transition-[filter]">
-                <AspectRatio ratio={21 / 9}>
-                  <Image
-                    src={
-                      'https://psychedelicalpha.com/wp-content/uploads/2023/10/Psychedelic-Alpha-Home.png'
-                    }
-                    fill
-                    alt="Image"
-                    sizes="(max-width: 768px) 80vw, (max-width: 1200px) 50vw, 20vw"
-                    className="rounded-md object-cover object-center"
-                  />
-                </AspectRatio>
-              </div>
-              <p className="line-clamp-2 text-muted-foreground text-base group-hover:text-foreground transition-colors">
-                {/* {description} */}
-                des iqwoej ijqweio jwqioej iowqjei wjeio jwqei jwioqje iowjeioqwjei ojwioe jqiojeio
-                ej
-              </p>
-            </CardContent>
-            <CardFooter>
-              <div className="relative flex gap-1.5 items-center text-muted-foreground">
-                {/* <Image
-                  src={'https://google.com'}
-                  alt="Image"
-                  width={20}
-                  height={20}
-                  className="object-contain"
-                /> */}
-                <Link className="text-sm break-all" href={'https://google.com'}>
-                  {'https://google.com'}
-                </Link>
-              </div>
-            </CardFooter>
-          </Card>
+          <GridCard slug={slug} title={title} url={url} type={type} language={language} />
           <div className="absolute bg-foreground w-[calc(100%-5px)] h-[calc(100%-5px)] top-1 left-1 rounded-lg -z-10"></div>
         </div>
       ))}
--- src/components/StackLayoutCard.tsx
+++ src/components/StackLayoutCard.tsx
@@ -1,78 +1,72 @@
-import clsx from 'clsx'
 import Image from 'next/image'
 import Link from 'next/link'
-
-import { AspectRatio } from './ui/aspect-ratio'
 
 import { getAllRecords } from '@/api/general'
 import { Badge } from '@/components/ui/badge'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { resolveMetaTag } from '@/utilities/metaTag'
 
+const StackCard = async ({ slug, title, url, type, language }) => {
+  const meta = await resolveMetaTag(url, slug)
+
+  return (
+    <Card className="relative overflow-hidden grid grid-cols-[minmax(0,1fr)_300px] md:grid-cols-[minmax(0,1fr)_200px] xl:grid-cols-[minmax(0,1fr)_320px] sm:group-hover:-translate-x-2 sm:group-hover:-translate-y-2 lg:group-hover:-translate-x-3 lg:group-hover:-translate-y-3 transition-transform z-10">
+      <div>
+        <CardHeader>
+          <CardTitle className="leading-normal mb-2">{title}</CardTitle>
+          <div className="flex gap-2">
+            <Badge className="w-fit" variant="secondary">
+              {type}
+            </Badge>
+            <Badge className="w-fit" variant="secondary">
+              {language}
+            </Badge>
+          </div>
+        </CardHeader>
+        <CardContent className="gap-2">
+          <p className="line-clamp-2 text-muted-foreground text-base group-hover:text-foreground transition-colors">
+            {meta.description}
+          </p>
+        </CardContent>
+        <CardFooter>
+          <div className="relative flex gap-1.5 items-center text-muted-foreground">
+            <Image
+              src={meta.iconUrl}
+              alt={`Icon of ${title}`}
+              width={20}
+              height={20}
+              className="object-contain"
+            />
+            <Link className="text-sm break-all" href={url}>
+              {url}
+            </Link>
+          </div>
+        </CardFooter>
+      </div>
+      <div className="saturate-[0.15] group-hover:saturate-100 transition-[filter]">
+        <Image
+          src={meta.imgUrl}
+          alt={`OG Image of ${url}`}
+          fill
+          className="object-cover object-center"
+        />
+      </div>
+    </Card>
+  )
+}
+
 const StackLayoutCard = async ({ params }) => {
   const records = await getAllRecords()
   const filterRecord = records.filter((record) => record.category === params.category)
 
-  // const meta = await resolveMetaTag(href, slug)
-
   return (
     <>
-      {filterRecord.map(({ category, slug, title, url, id, type, language }) => (
-        <div className="h-fit">
+      {filterRecord.map(({ slug, title, url, id, type, language }) => (
+        <div key={id} className="h-fit">
           <div className="relative group">
-            <Card className="relative overflow-hidden grid grid-cols-[minmax(0,1fr)_300px] md:grid-cols-[minmax(0,1fr)_200px] xl:grid-cols-[minmax(0,1fr)_320px] sm:group-hover:-translate-x-2 sm:group-hover:-translate-y-2 lg:group-hover:-translate-x-3 lg:group-hover:-translate-y-3 transition-transform z-10">
-              <div>
-                <CardHeader>
-                  <CardTitle className="leading-normal mb-2">{title}</CardTitle>
-                  <div className="flex gap-2">
-                    <Badge className="w-fit" variant="secondary">
-                      {type}
-                    </Badge>
-                    <Badge className="w-fit" variant="secondary">
-                      {language}
-                    </Badge>
-                  </div>
-                  <div>Description goes here</div>
-                  <div className="">Links goes here</div>
-                </CardHeader>
-                <CardContent className="flex gap-2">
-                  <div className="saturate-[0.15] group-hover:saturate-100 transition-[filter]">
-                    <AspectRatio ratio={21 / 9}>
-                      <Image
-                        src={
-                          'https://psychedelicalpha.com/wp-content/uploads/2023/10/Psychedelic-Alpha-Home.png'
-                        }
-                        fill
-                        alt="Image"
-                        sizes="(max-width: 768px) 80vw, (max-width: 1200px) 50vw, 20vw"
-                        className="rounded-md object-cover object-center"
-                      />
-                    </AspectRatio>
-                  </div>
-                </CardContent>
-              </div>
-              <div className="relative">
-                <Image
-                  src={
-                    'https://psychedelicalpha.com/wp-content/uploads/2023/10/Psychedelic-Alpha-Home.png'
-                  }
-                  alt="Image"
-                  fill
-                  className="object-cover  object-center"
-                />
-              </div>
-            </Card>
+            <StackCard slug={slug} title={title} url={url} type={type} language={language} />
             <div className="absolute bg-foreground w-[calc(100%-5px)] h-[calc(100%-5px)] top-1 left-1 rounded-lg -z-10"></div>
           </div>
-          {/* <Link
-        href={`/database/${category}/${slug}`}
-        className={clsx(
-          badgeVariants({ variant: 'secondary' }),
-          'mt-2 hover:opacity-60 transition-opacity rounded-sm',
-        )}
-      >
-        {title}
-      </Link> */}
         </div>
       ))}
     </>
